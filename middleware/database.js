9faa41283b9b3908bf3985629e4fa565d7297d6c
Niyi Enny <myjessy.sr@gmail.com>
2022-07-10 06:44:42 +0100

    JSK

--- middleware/database.js
+++ middleware/database.js
@@ -1,6 +1,8 @@
 import { MongoClient } from 'mongodb';
 import nextConnect from 'next-connect';
-const uri = "mongodb://dadmin:34Ether34%@127.0.0.1:27017/?authSource=admin&retryWrites=true&w=majority";
+const username = encodeURIComponent("dadmin");
+const password = encodeURIComponent("34Ether34%");
+const uri = `mongodb://${username}:${password}@127.0.0.1:27017/?authSource=admin&authMechanism=DEFAULT`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
